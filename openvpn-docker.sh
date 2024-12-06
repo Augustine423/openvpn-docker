f9f586c0a59d6fa086b9587e757ead3e067fe7e0
Kyaw Zay Yar Soe <kyawzayarsoe@gmail.com>
2024-12-06 21:59:51 +0630

    Update openvpn-docker.sh

--- openvpn-docker.sh
+++ openvpn-docker.sh
@@ -15,19 +15,33 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 
 # 2. OpenVPN Docker Image Installation and Run
 docker pull openvpn/openvpn-as
+
+# Prompt user for port numbers
+echo "Default ports are:"
+echo " - Web interface: 943"
+echo " - SSL: 443"
+echo " - UDP: 1194"
+
+read -p 'Would you like to change the default ports? (y/n): ' change_ports
+if [[ "$change_ports" =~ ^[Yy]$ ]]; then
+    read -p 'Enter port for web interface (default 943): ' PORT_943
+    PORT_943=${PORT_943:-943}
+    read -p 'Enter port for SSL (default 443): ' PORT_443
+    PORT_443=${PORT_443:-443}
+    read -p 'Enter port for UDP (default 1194): ' PORT_1194
+    PORT_1194=${PORT_1194:-1194}
+else
+    PORT_943=943
+    PORT_443=443
+    PORT_1194=1194
+fi
+
+# Run the OpenVPN container with the specified ports
 docker run -d \
   --name=openvpn-as --cap-add=NET_ADMIN \
-  -p 943:943 -p 443:443 -p 1194:1194/udp \
+  -p $PORT_943:943 -p $PORT_443:443 -p $PORT_1194:1194/udp \
   -v /path/to/data:/openvpn \
   openvpn/openvpn-as
-
-# Prompt for port numbers
-read -p 'Enter port for web interface (default 943): ' PORT_943
-PORT_943=${PORT_943:-943}
-read -p 'Enter port for SSL (default 443): ' PORT_443
-PORT_443=${PORT_443:-443}
-read -p 'Enter port for UDP (default 1194): ' PORT_1194
-PORT_1194=${PORT_1194:-1194}
 
 # 3. Display Information
 echo -e "\e[32mOpenVPN Access Server is running.\e[0m"
