9793d431f96d2995a8c6f1fcd9d232ad89d4e9a8
Kyaw Zay Yar Soe <kyawzayarsoe@gmail.com>
2024-12-06 21:55:57 +0630

    Update openvpn-docker.sh

--- openvpn-docker.sh
+++ openvpn-docker.sh
@@ -13,6 +13,14 @@ $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/s
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# 2. OpenVPN Docker Image Installation and Run
+docker pull openvpn/openvpn-as
+docker run -d \
+  --name=openvpn-as --cap-add=NET_ADMIN \
+  -p 943:943 -p 443:443 -p 1194:1194/udp \
+  -v /path/to/data:/openvpn \
+  openvpn/openvpn-as
+
 # Prompt for port numbers
 read -p 'Enter port for web interface (default 943): ' PORT_943
 PORT_943=${PORT_943:-943}
@@ -20,14 +28,6 @@ read -p 'Enter port for SSL (default 443): ' PORT_443
 PORT_443=${PORT_443:-443}
 read -p 'Enter port for UDP (default 1194): ' PORT_1194
 PORT_1194=${PORT_1194:-1194}
-
-# 2. OpenVPN Docker Image Installation and Run
-docker pull openvpn/openvpn-as
-docker run -d \
-  --name=openvpn-as --cap-add=NET_ADMIN \
-  -p $PORT_943:943 -p $PORT_443:443 -p $PORT_1194:1194/udp \
-  -v /path/to/data:/openvpn \
-  openvpn/openvpn-as
 
 # 3. Display Information
 echo -e "\e[32mOpenVPN Access Server is running.\e[0m"
